ef19571f0b8a2d96263d802f376f75b2a97cde0c
赵亚锋 <zhaoyafeng@meituan.com>
2017-04-08 14:04:19 +0800

    实现初步接口

--- persistent/persistent.go
+++ persistent/persistent.go
@@ -1,11 +1,56 @@
 package persistent
 
-import "github.com/golang/exp/mmap"
+import (
+	"bytes"
+	"encoding/gob"
+	"os"
+
+	"github.com/edsrzf/mmap-go"
+)
 
 type Map struct {
-	reader mmap.ReaderAt
+	mem       mmap.MMap
+	memData   map[interface{}]interface{}
+	totalSize int
+	keySize   int
+	valSize   int
+	index     int
 }
 
-func (m *Map) NewMap(keySize, valSize int) {
+func NewMap(keySize, valSize, totalSize int, filename string) (MMap, error) {
+	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
 
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, keySize+valSize, totalSize)
+	writeSize, err := f.Write(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	mem, err := mmap.Map(f,  0, 0)
+	if err != nil {
+		return nil, err
+	}
+	memData = make(map[interface{}]interface{}, totalSize/(keySize+valSize))
+	return mem, err
+}
+
+func (m *Map) Get(key interface{}) (interface{}, error) {
+
+}
+
+// todo: 返回接口 应有 告诉 是否 成功更新
+func (m *Map) Set(key, val interface{}) error {
+	memData[key] = val
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(key)
+	if err != nil {
+		return err
+	}
+	for i := index; i < m.keySize; ++i {
+		m.mem[i] = buf.Bytes()
+	}
 }
